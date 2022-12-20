b4bd14baedf1099e3bda3dda200da61b1d7bf1e6
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2022-12-20 11:20:11 -0600

    refactor: add BaseModel to User model
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- wapl/wapl/users/apps.py
+++ wapl/wapl/users/apps.py
@@ -1,3 +1,6 @@
+"""Users app."""
+
+# Django
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
--- wapl/wapl/users/models.py
+++ wapl/wapl/users/models.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from wapl.utils.models import BaseModel
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
+class User(BaseModel, AbstractUser):
     """
     Default custom user model for WiFi access point location.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
 
     #: First and last name do not cover name patterns around the globe
