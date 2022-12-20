f5264529dc5bd615bc05ed7a201bf6b4e86152fd
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2022-12-20 11:22:03 -0600

    feat: add Hotspot app
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ wapl/wapl/hotspots/admin.py
@@ -0,0 +1,24 @@
+"""Hotspots admin."""
+
+# Django
+from django.contrib import admin
+
+# Model
+from wapl.hotspots.models import Hotspot
+
+
+@admin.register(Hotspot)
+class HotspotAdmin(admin.ModelAdmin):
+    """Hotspot admin."""
+
+    list_display = (
+        'name',
+        'program',
+        'installed_date',
+        'lat',
+        'long',
+        'address',
+        'mayoralty'
+    )
+    search_fields = ('name',)
+    list_filter = ('program', 'mayoralty')
---
+++ wapl/wapl/hotspots/models/hotspots.py
@@ -0,0 +1,62 @@
+"""Hotspots model."""
+
+# Django
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Utilities
+from wapl.utils.models import BaseModel
+import math
+
+
+class Hotspot(BaseModel):
+    """Hotsport model.
+    A hotsport wifi is a wifi access point locate in a site.  
+    """
+
+    name = models.CharField(
+        verbose_name=_("Hotspot ID"),
+        max_length=130,
+        unique=True
+    )
+
+    program = models.CharField(
+        verbose_name=_("Program"),
+        max_length=60,
+    )
+
+    installed_date = models.DateField(
+        verbose_name=_("Installed date")
+    )
+
+    lat = models.FloatField(
+        verbose_name=_("Latitude"),
+        blank=False
+    )
+
+    long = models.FloatField(
+        verbose_name=_("Longitude"),
+        blank=False
+    )
+
+    address = models.CharField(
+        verbose_name=_("Address"),
+        max_length=100,
+        null=True,
+        blank=False,
+    )
+
+    mayoralty = models.CharField(
+        verbose_name=_("Mayoralty"),
+        max_length=100,
+        blank=False
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    def distance(self, lat: float, long: float) -> float:
+        return math.sqrt(
+            (lat - self.lat) ** 2 +
+            (long - self.long)**2
+        )
